4aed40980bf95a05e665787474deab461d9554fd
jardeldyonisio <jardel.dyonisio@hotmail.com>
2025-09-04 20:10:17 -0300

    :fire: removed matlab files

--- matlab/cpoints.m
+++
@@ -1,4 +0,0 @@
-function C = cpoints(x)
-
-[P,ang,dt] = ktangdt(x); % Separate vector x.
-C = ctpts(P,ang,dt); % Call to compute the control points.
--- matlab/knots.m
+++
@@ -1,8 +0,0 @@
-function P = knots(Q,k)
-
-% function P = knots(Q,k). This function takes data points Q
-% and knot sequence vector k and picks out the knot points
-% of the curve.
-
-P= []; 
-P= [P Q(:,k)];
